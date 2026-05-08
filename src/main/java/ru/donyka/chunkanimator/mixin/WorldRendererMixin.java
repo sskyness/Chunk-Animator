9cb13f8922554b0226ea84a8aa8101916fdde855
Donyka <haruclient@gmail.com>
2026-05-08 21:57:14 +0300

    Port Chunk Animator to Fabric 1.21.4

--- src/main/java/ru/donyka/chunkanimator/mixin/WorldRendererMixin.java
+++ src/main/java/ru/donyka/chunkanimator/mixin/WorldRendererMixin.java
@@ -3,31 +3,30 @@ package ru.donyka.chunkanimator.mixin;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.render.chunk.ChunkBuilder;
-import org.joml.Vector3f;
-import org.joml.Vector3fc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import ru.donyka.chunkanimator.AnimationOffset;
 import ru.donyka.chunkanimator.ChunkAnimator;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
-    @ModifyArg(
-            method = "renderBlockLayers",
+    @ModifyArgs(
+            method = "renderLayer",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gl/DynamicUniforms$UniformValue;<init>(Lorg/joml/Matrix4fc;Lorg/joml/Vector4fc;Lorg/joml/Vector3fc;Lorg/joml/Matrix4fc;F)V"
-            ),
-            index = 2
+                    target = "Lnet/minecraft/client/gl/GlUniform;set(FFF)V",
+                    ordinal = 0
+            )
     )
-    private Vector3fc chunkanimator$offsetChunk(Vector3fc original, @Local ChunkBuilder.BuiltChunk builtChunk) {
+    private void chunkanimator$offsetChunk(Args args, @Local ChunkBuilder.BuiltChunk builtChunk) {
         AnimationOffset offset = ChunkAnimator.animationHandler().offsetFor(builtChunk);
 
-        if (offset.isZero()) {
-            return original;
+        if (!offset.isZero()) {
+            args.set(0, ((Float) args.get(0)) + offset.x());
+            args.set(1, ((Float) args.get(1)) + offset.y());
+            args.set(2, ((Float) args.get(2)) + offset.z());
         }
-
-        return new Vector3f(original).add(offset.x(), offset.y(), offset.z());
     }
 }
