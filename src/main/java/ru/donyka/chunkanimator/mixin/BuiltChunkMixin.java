09230725b96a19f75bfb3beb8aa78c97734412b0
Donyka <haruclient@gmail.com>
2026-05-08 21:58:39 +0300

    Port Chunk Animator to Fabric 1.21.1

--- src/main/java/ru/donyka/chunkanimator/mixin/BuiltChunkMixin.java
+++ src/main/java/ru/donyka/chunkanimator/mixin/BuiltChunkMixin.java
@@ -1,6 +1,7 @@
 package ru.donyka.chunkanimator.mixin;
 
 import net.minecraft.client.render.chunk.ChunkBuilder;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,10 +10,8 @@ import ru.donyka.chunkanimator.ChunkAnimator;
 
 @Mixin(ChunkBuilder.BuiltChunk.class)
 public abstract class BuiltChunkMixin {
-    @Inject(method = "setSectionPos", at = @At("TAIL"))
-    private void chunkanimator$setSectionPos(long sectionPos, CallbackInfo ci) {
-        ChunkBuilder.BuiltChunk builtChunk = (ChunkBuilder.BuiltChunk) (Object) this;
-        ChunkAnimator.animationHandler().setOrigin(builtChunk, builtChunk.getOrigin());
+    @Inject(method = "setOrigin", at = @At("TAIL"))
+    private void chunkanimator$setOrigin(int x, int y, int z, CallbackInfo ci) {
+        ChunkAnimator.animationHandler().setOrigin((ChunkBuilder.BuiltChunk) (Object) this, new BlockPos(x, y, z));
     }
 }
-
