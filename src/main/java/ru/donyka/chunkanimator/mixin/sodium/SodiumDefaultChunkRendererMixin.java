1d548d6505d7fb2051b919f4eeda56a6f7ae0587
Donyka <haruclient@gmail.com>
2026-05-09 22:39:31 +0300

    Fix Sodium chunk animation offsets

--- src/main/java/ru/donyka/chunkanimator/mixin/sodium/SodiumDefaultChunkRendererMixin.java
+++ src/main/java/ru/donyka/chunkanimator/mixin/sodium/SodiumDefaultChunkRendererMixin.java
@@ -1,13 +1,12 @@
 package ru.donyka.chunkanimator.mixin.sodium;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import ru.donyka.chunkanimator.AnimationOffset;
-import ru.donyka.chunkanimator.ChunkAnimator;
+import ru.donyka.chunkanimator.compat.SodiumRenderRegionBridge;
 
 @Pseudo
 @Mixin(targets = "net.caffeinemc.mods.sodium.client.render.chunk.DefaultChunkRenderer", remap = false)
@@ -22,41 +21,51 @@ public abstract class SodiumDefaultChunkRendererMixin {
                     "Lnet/caffeinemc/mods/sodium/client/render/viewport/CameraTransform;" +
                     "Lnet/caffeinemc/mods/sodium/client/gl/buffer/GlBuffer;)V";
 
-    @ModifyArgs(
-            method = SET_MODEL_MATRIX_UNIFORMS,
+    @Redirect(
+            method = {SET_MODEL_MATRIX_UNIFORMS, SET_MODEL_MATRIX_UNIFORMS_WITH_CHUNK_DATA},
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/shader/ChunkShaderInterface;setRegionOffset(FFF)V",
+                    target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegion;getOriginX()I",
                     remap = false
             ),
             remap = false,
             require = 0
     )
-    private static void chunkanimator$offsetRegion(Args args, @Local(argsOnly = true, ordinal = 1) Object region) {
-        chunkanimator$applyOffset(args, region);
+    private static int chunkanimator$getOriginX(@Coerce Object region) {
+        SodiumRenderRegionBridge bridge = (SodiumRenderRegionBridge) region;
+        AnimationOffset offset = bridge.chunkanimator$animationOffset();
+        return bridge.chunkanimator$originX() + Math.round(offset.x());
     }
 
-    @ModifyArgs(
-            method = SET_MODEL_MATRIX_UNIFORMS_WITH_CHUNK_DATA,
+    @Redirect(
+            method = {SET_MODEL_MATRIX_UNIFORMS, SET_MODEL_MATRIX_UNIFORMS_WITH_CHUNK_DATA},
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/shader/ChunkShaderInterface;setRegionOffset(FFF)V",
+                    target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegion;getOriginY()I",
                     remap = false
             ),
             remap = false,
             require = 0
     )
-    private static void chunkanimator$offsetRegionWithChunkData(Args args, @Local(argsOnly = true, ordinal = 1) Object region) {
-        chunkanimator$applyOffset(args, region);
+    private static int chunkanimator$getOriginY(@Coerce Object region) {
+        SodiumRenderRegionBridge bridge = (SodiumRenderRegionBridge) region;
+        AnimationOffset offset = bridge.chunkanimator$animationOffset();
+        return bridge.chunkanimator$originY() + Math.round(offset.y());
     }
 
-    private static void chunkanimator$applyOffset(Args args, Object region) {
-        AnimationOffset offset = ChunkAnimator.animationHandler().offsetFor(region);
-
-        if (!offset.isZero()) {
-            args.set(0, ((Float) args.get(0)) + offset.x());
-            args.set(1, ((Float) args.get(1)) + offset.y());
-            args.set(2, ((Float) args.get(2)) + offset.z());
-        }
+    @Redirect(
+            method = {SET_MODEL_MATRIX_UNIFORMS, SET_MODEL_MATRIX_UNIFORMS_WITH_CHUNK_DATA},
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegion;getOriginZ()I",
+                    remap = false
+            ),
+            remap = false,
+            require = 0
+    )
+    private static int chunkanimator$getOriginZ(@Coerce Object region) {
+        SodiumRenderRegionBridge bridge = (SodiumRenderRegionBridge) region;
+        AnimationOffset offset = bridge.chunkanimator$animationOffset();
+        return bridge.chunkanimator$originZ() + Math.round(offset.z());
     }
 }
--- src/main/java/ru/donyka/chunkanimator/mixin/sodium/legacy/LegacySodiumDefaultChunkRendererMixin.java
+++ src/main/java/ru/donyka/chunkanimator/mixin/sodium/legacy/LegacySodiumDefaultChunkRendererMixin.java
@@ -1,13 +1,12 @@
 package ru.donyka.chunkanimator.mixin.sodium.legacy;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import ru.donyka.chunkanimator.AnimationOffset;
-import ru.donyka.chunkanimator.ChunkAnimator;
+import ru.donyka.chunkanimator.compat.SodiumRenderRegionBridge;
 
 @Pseudo
 @Mixin(targets = "me.jellysquid.mods.sodium.client.render.chunk.DefaultChunkRenderer", remap = false)
@@ -17,23 +16,51 @@ public abstract class LegacySodiumDefaultChunkRendererMixin {
                     "Lme/jellysquid/mods/sodium/client/render/chunk/region/RenderRegion;" +
                     "Lme/jellysquid/mods/sodium/client/render/viewport/CameraTransform;)V";
 
-    @ModifyArgs(
+    @Redirect(
             method = SET_MODEL_MATRIX_UNIFORMS,
             at = @At(
                     value = "INVOKE",
-                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/shader/ChunkShaderInterface;setRegionOffset(FFF)V",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/region/RenderRegion;getOriginX()I",
                     remap = false
             ),
             remap = false,
             require = 0
     )
-    private static void chunkanimator$offsetRegion(Args args, @Local(argsOnly = true, ordinal = 1) Object region) {
-        AnimationOffset offset = ChunkAnimator.animationHandler().offsetFor(region);
+    private static int chunkanimator$getOriginX(@Coerce Object region) {
+        SodiumRenderRegionBridge bridge = (SodiumRenderRegionBridge) region;
+        AnimationOffset offset = bridge.chunkanimator$animationOffset();
+        return bridge.chunkanimator$originX() + Math.round(offset.x());
+    }
 
-        if (!offset.isZero()) {
-            args.set(0, ((Float) args.get(0)) + offset.x());
-            args.set(1, ((Float) args.get(1)) + offset.y());
-            args.set(2, ((Float) args.get(2)) + offset.z());
-        }
+    @Redirect(
+            method = SET_MODEL_MATRIX_UNIFORMS,
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/region/RenderRegion;getOriginY()I",
+                    remap = false
+            ),
+            remap = false,
+            require = 0
+    )
+    private static int chunkanimator$getOriginY(@Coerce Object region) {
+        SodiumRenderRegionBridge bridge = (SodiumRenderRegionBridge) region;
+        AnimationOffset offset = bridge.chunkanimator$animationOffset();
+        return bridge.chunkanimator$originY() + Math.round(offset.y());
+    }
+
+    @Redirect(
+            method = SET_MODEL_MATRIX_UNIFORMS,
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/region/RenderRegion;getOriginZ()I",
+                    remap = false
+            ),
+            remap = false,
+            require = 0
+    )
+    private static int chunkanimator$getOriginZ(@Coerce Object region) {
+        SodiumRenderRegionBridge bridge = (SodiumRenderRegionBridge) region;
+        AnimationOffset offset = bridge.chunkanimator$animationOffset();
+        return bridge.chunkanimator$originZ() + Math.round(offset.z());
     }
 }
